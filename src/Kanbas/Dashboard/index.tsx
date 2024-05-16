125c06b8e35e0bb05b6e28725d1b0f40a1c18f30
lihaohl0307 <lihao@lihaodeMacBook-Pro.local>
2024-05-16 11:18:40 -0700

    update labs

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -16,8 +16,74 @@ export default function Dashboard() {
               </p>
               <a href="#/Kanbas/Courses/1234/Home"> Go </a>
             </div>
-            <div className="wd-dashboard-course"> ... </div>
-            <div className="wd-dashboard-course"> ... </div>
+            <br/>
+            <img src="images/mobileapp.png" width={200}/>
+            <div className="wd-dashboard-course"> 
+              <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home">
+                  CS5520 Mobile App Development
+                </a>
+                <p className="wd-dashboard-course-title">
+                  android studio
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+            <br/>
+            <img src="images/ML.png" width={200}/>
+            <div className="wd-dashboard-course"> 
+              <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home">
+                  CS6140 Machine Learning
+                </a>
+                <p className="wd-dashboard-course-title">
+                  Machine Learning
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+            <img src="images/algorithms.png" width={200}/>
+            <div className="wd-dashboard-course"> 
+              <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home">
+                  CS5800 Algorithms
+                </a>
+                <p className="wd-dashboard-course-title">
+                  Algo
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+            <img src="images/OOP.png" width={200}/>
+            <div className="wd-dashboard-course"> 
+              <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home">
+                  CS5007 Object Oriented Programming
+                </a>
+                <p className="wd-dashboard-course-title">
+                Object Oriented Programming
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+            <img src="images/python.png" width={200}/>
+            <div className="wd-dashboard-course"> 
+              <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home">
+                  CS5000 Python
+                </a>
+                <p className="wd-dashboard-course-title">
+                  Python
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+            <img src="images/distribution.png" width={200}/>
+            <div className="wd-dashboard-course"> 
+              <a className="wd-dashboard-course-link"
+                  href="#/Kanbas/Courses/1234/Home">
+                  CS5700 Distributed System
+                </a>
+                <p className="wd-dashboard-course-title">
+                 Distributed System
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
           </div>
         </div>
       </div>
