dbcee6ff600accf1f1e62bbed2333668ba18bd07
lihaohl0307 <lihaohl0307@gmail.com>
2024-06-29 13:01:36 -0700

    Dashboard-useState

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -4,18 +4,32 @@ import * as db from "../Database";
 
 export default function Dashboard() {
     const [courses, setCourses] = useState(db.courses);
-    const course: any = {
+    const [course, setCourse] = useState<any>({
       _id: "0", name: "New Course", number: "New Number",
       startDate: "2023-09-10", endDate: "2023-12-15",
       image: "/images/reactjs.jpg", description: "New Description"
-    };
+    });
     const addNewCourse = () => {
       const newCourse = {
-        ...course, _id : new Date().getTime().toString()};
+        ...course, name: `${course.name} ${courses.length}`,_id : new Date().getTime().toString()};
         setCourses([newCourse, ...courses]);
     };
+    const deleteCourse = (courseId: string) => {
+      setCourses(courses.filter((course) => course._id !== courseId));
+    };
+    const updateCourse = () => {
+      setCourses(
+        courses.map((c) => {
+          if (c._id === course._id) {
+            return course;
+          } else {
+            return c;
+          }
+        })
+      );
+    };
 
-     return (
+    return (
       <div id="wd-dashboard">
         <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
         <h5>New Course
@@ -23,7 +37,16 @@ export default function Dashboard() {
               id="wd-add-new-course-click"
               onClick={addNewCourse}> 
               Add</button>
-        </h5>
+              <button className="btn btn-warning float-end me-2"
+                onClick={updateCourse} id="wd-update-course-click">
+                Update
+              </button>
+        </h5><hr />
+        <input value={course.name} className="form-control mb-2"
+                onChange={(e) => setCourse({ ...course, name: e.target.value }) }/>
+        <textarea value={course.description} className="form-control"
+                 onChange={(e) => setCourse({ ...course, description: e.target.value }) }/><hr />
+
         <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
         <div id="wd-dashboard-courses" className="row" >
           <div className="row row-cols-1 row-cols-md-4 row-cols-lg-6 row-cols-xl-8">
@@ -41,6 +64,22 @@ export default function Dashboard() {
                         {course.description}
                       </p>
                       <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">Go</Link>
+                      <button onClick={(event) => {
+                                        event.preventDefault(); // you want to perform some action without submitting the form, you call event.preventDefault() to prevent the form submission.
+                                        deleteCourse(course._id);}} 
+                              className="btn btn-danger float-end"
+                              id="wd-delete-course-click">
+                              Delete
+                      </button>
+                      <button id="wd-edit-course-click"
+                              onClick={(event) => {
+                                event.preventDefault();
+                                setCourse(course);
+                              }}
+                              className="btn btn-warning me-2 float-end" >
+                              Edit
+                      </button>
+
                     </div>
                   </div>
                 </Link>
