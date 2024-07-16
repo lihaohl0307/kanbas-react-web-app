7c9cc5948650a6df9f450d50250babd1bad0d4d3
lihaohl0307 <lihaohl0307@gmail.com>
2024-07-15 23:14:25 -0700

    working with objects

---
+++ src/Labs/Lab5/index.tsx
@@ -0,0 +1,20 @@
+import PathParameters from "../Lab5/PathParameters";
+import EnvironmentVariables from "./EnvironmentVariables";
+import QueryParameters from "./QueryParameters";
+import WorkingWithObjects from "./WorkingWithObjects";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+export default function Lab5() {
+    return (
+      <div id="wd-lab5">
+        <h2>Lab 5</h2>
+          <a href={`${REMOTE_SERVER}/lab5`}>
+             <h3>Go to lab5</h3>
+          </a><hr />
+          <EnvironmentVariables />
+          <PathParameters />
+          <QueryParameters />
+          <WorkingWithObjects />
+      </div>
+    );
+  }
+  
