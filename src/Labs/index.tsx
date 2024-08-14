1e5e90261f6c0031b14f4e6ceb5814d486353306
lihaohl0307 <lihaohl0307@gmail.com>
2024-08-14 15:18:04 -0700

    add repo link

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,13 +7,19 @@ import Lab4 from "./Lab4";
 import { Provider } from "react-redux";
 import store from "./store";
 import Lab5 from "./Lab5";
+import { Link } from "react-router-dom";
 export default function Labs() {
   return (
     <Provider store={store}>
       <div>
         <h1>Labs</h1>
         <h3>Full Name: Hao Li</h3>
-          <TOC />
+        <h3>Team member: Hao Li</h3>
+        <h4><a href="https://github.com/lihaohl0307/kanbas-react-web-app.git">Github-React</a></h4>
+        <h4><a href="https://github.com/lihaohl0307/kanbas-node-server-app.git">Github-Node</a></h4>
+        <h4><a href="https://kanbas-node-server-app-1-gdpv.onrender.com">Render</a></h4>
+
+        <TOC />
         <Routes>
           <Route path="/" element={<Navigate to="Lab1" />} />
           <Route path="Lab1" element={<Lab1 />} />
